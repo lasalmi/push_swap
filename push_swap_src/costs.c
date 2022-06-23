c0e4ff8dfe11cdb9a23bb73c150c74736d94cb54
Lauri Salmi <late.salmi@gmail.com>
2022-06-23 13:11:54 +0300

    Adding cost counters

--- push_swap_src/costs.c
+++ push_swap_src/costs.c
@@ -6,7 +6,7 @@
 /*   By: lasalmi <lasalmi@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 12:25:08 by lasalmi           #+#    #+#             */
-/*   Updated: 2022/06/23 12:49:25 by lasalmi          ###   ########.fr       */
+/*   Updated: 2022/06/23 13:11:41 by lasalmi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ t_cost	ft_count_cost(int member, int i, int stack_size)
 {
 	t_cost	ret;
 
+	ret.found = 1;
 	ret.target = member;
 	ret.rotate_cost = i;
 	ret.rev_cost = stack_size - i;
@@ -48,4 +49,43 @@ size_t	ft_total_cost_move(t_cost target_a, t_cost target_b)
 	if (rotate_cost < rev_cost)
 		return (rotate_cost);
 	return (rev_cost);
+}
+
+void	ft_initialize_target(t_cost	*cost)
+{
+	cost->target = 0;
+	cost->rev_cost = 0;
+	cost->rotate_cost = 0;
+	cost->found = 0;
+}
+
+/* Finds the nearest smaller number and if no smaller numbers are found
+returns the biggest target in the stack */
+t_cost	ft_find_pair(int stack_a, t_node *head, int list_size)
+{
+	t_cost	ret;
+	int		i;
+	int		diff;
+
+	i = 0;
+	diff = INT_MAX;
+	ft_initialize_target(&ret);
+	if (!head)
+		return (ret);
+	while (head)
+	{
+		if ((stack_a - head->value) > 0 && (stack_a - head->value) < diff)
+		{
+			diff = stack_a - head->value;
+			ret = ft_count_cost(head->value, i, list_size);
+		}
+		i++;
+		head = head->next;
+	}
+	/* IF !FOUND handle biggest */
+	return (ret);
+}
+t_pair	ft_cheapest_pair(t_utils utils, t_chunk *chunk)
+{
+	while (utils->head_)
 }
